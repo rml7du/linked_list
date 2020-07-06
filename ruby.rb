a2506dcae4ed03e9d773785c8c3dfff8efe19cd2
Robert Lee <rml7du@gmail.com>
2020-07-06 09:24:51 -0600

    initial commit with structure of project

---
+++ ruby.rb
@@ -0,0 +1,69 @@
+class LinkedList
+    def initialize()
+
+    end
+
+    def append(value) #adds a new node contain value at end of list
+    end
+
+    def prepend(value) #adds a new node containing value at begining of list
+
+    end
+
+    def size() #returns totla number of nodes
+
+    end
+
+    def head() #returns the first node in the list
+
+    end
+
+    def tail() #returns the last node in the list
+
+    end
+
+    def at(index) #returns the node at the given index
+
+    end
+
+    def pop() #removes the last element from the list
+   
+    end
+
+    def contains?(value) #returns true if the passed in value is in the list
+
+    end
+
+    def find(value) #returns the index of the node containing value or nil if not found
+
+    end
+
+    def to_s() #represent your LinkedList objects as strings, so you can pring them out 
+        # and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+    
+    end
+
+    def insert_at(value, index) #extra credit insert value at index
+
+    end
+
+    def remove_at(index) #extra credit to remove node at index, make sure up date links of nodes
+
+    end
+    
+end
+
+class Node
+    def initialize()
+        @value = nil
+        @next_node = nil
+    end
+
+    def value()
+
+    end
+
+    def next_node()
+
+    end
+end
